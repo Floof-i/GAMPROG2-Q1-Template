325e8524d0bec368a1883fc3c5f452cf451bf2fc
Floof-i <116511585+Floof-i@users.noreply.github.com>
2023-06-15 00:28:53 +0800

    add id detection and ui text change

--- Assets/Scripts/Interaction/RaycastController.cs
+++ Assets/Scripts/Interaction/RaycastController.cs
@@ -10,9 +10,11 @@ public class RaycastController : MonoBehaviour
 
     [SerializeField]
     //The layer that will determine what the raycast will hit
-    LayerMask layerMask;
+    LayerMask interactable;
     //The UI text component that will display the name of the interactable hit
     public TextMeshProUGUI interactionInfo;
+
+    private bool interactableHover = false;
 
     // Update is called once per frame
     private void Update()
@@ -23,5 +25,24 @@ public class RaycastController : MonoBehaviour
         //2. Check if the object hits any Interactable. If it does, show the interactionInfo and set its text
         //   to the id of the Interactable hit. If it doesn't hit any Interactable, simply disable the text
         //3. Make sure to interact with the Interactable only when the mouse button is pressed.
+
+        RaycastHit hit;
+
+        interactableHover = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, raycastDistance, interactable);
+
+        if(interactableHover)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * raycastDistance, Color.red);
+            Debug.Log("Collided with interactable!");
+            interactionInfo.enabled = true;
+            Item item = hit.collider.GetComponent("Item") as Item;
+            interactionInfo.text = "Interact with " + item.id;
+        }
+        else if(!interactableHover)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * raycastDistance, Color.green);
+            Debug.Log("No interactable!");
+            interactionInfo.enabled = false;
+        }
     }
 }
